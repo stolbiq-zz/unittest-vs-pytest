362ea7a24ad3afee7a0e51a7dbf1944a6972b3da
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-11 00:15:45 +0200

    add pytest version of test_setup

---
+++ test_pytest/test_setup/test_1.py
@@ -0,0 +1,10 @@
+def test_db_save(self):
+    """Test db save"""
+    print('######## running save')
+
+def test_db_delete(self):
+    """Test db deletion"""
+    print('######## running delete')
+
+def test_non_db(self):
+    print('######## non db test')

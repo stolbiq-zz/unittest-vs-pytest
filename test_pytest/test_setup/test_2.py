24c5f60efd1978aa0882f56fecbaddf0ff50ca39
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-12 15:08:36 +0200

    Add session to test_2 for pytest setup_base

---
+++ test_pytest/test_setup/test_2.py
@@ -0,0 +1,10 @@
+def test_db_save(session):
+    """Test db save"""
+    print('######## running save 2')
+
+def test_db_delete(session):
+    """Test db deletion"""
+    print('######## running delete 2')
+
+def test_non_db():
+    print('######## non db test')

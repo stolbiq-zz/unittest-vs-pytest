889c8170ebd250fcf14d8e1d8a5ef4284e964269
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-17 23:48:50 +0200

    Remove file test for pytest with mocker from mock-pytest

---
+++ test_pytest/test_remove_file.py
@@ -0,0 +1,6 @@
+from remove_file import remove_file
+
+def test_remove_file(mocker):
+    mock_remove = mocker.patch('os.remove')
+    remove_file('file')
+    mock_remove.assert_called_once_with('file')

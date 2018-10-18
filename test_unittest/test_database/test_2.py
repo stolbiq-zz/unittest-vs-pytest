411fa186c27688b62901f830dddcaf26256bd791
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-19 00:40:52 +0200

    DataBase case, add test_2, how not to copy the code of setUp and tearDown?

---
+++ test_unittest/test_database/test_2.py
@@ -0,0 +1,6 @@
+class Setup2TestCase():
+    def test_data_base_save_2(self):
+        print('######## running save 2')
+
+    def test_data_base_delete_2(self):
+        print('######## running delete 2')

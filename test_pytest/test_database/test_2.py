e26df021ce331c0725e17f9972c263942ee22e3b
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-19 00:59:31 +0200

    PyTest: DataBase case, what if you have the second module that also needs a session?

--- test_pytest/test_database/test_2.py
+++ test_pytest/test_database/test_2.py
@@ -1,11 +1,4 @@
-import pytest
-from database_classes import Session, Connection
 
-@pytest.fixture
-def session():
-    session = Session()
-    yield session
-    session.close()
 
 def test_data_base_save(session):
     print('######## running save')

e2743e545d90eb201a593f59b6cdb54c1cce9fca
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-19 00:48:28 +0200

    PyTest: DataBase case, start with three functions in test_1

---
+++ test_pytest/test_database/test_1.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_data_base_save():
+    print('######## running save')
+
+def test_data_base_delete():
+    print('######## running delete')
+
+def test_non_data_base():
+    print('######## non database test')

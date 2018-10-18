ccff181561aa6415dd5dd9f0c4ad9a34b58ba4cb
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-19 00:44:59 +0200

    Unittest: DataBase case, what if we have a non database test in test_1

--- test_unittest/test_database/test_1.py
+++ test_unittest/test_database/test_1.py
@@ -4,9 +4,11 @@ from base import SetupBaseTestCase
 
 class Setup1TestCase(SetupBaseTestCase):
 
-
     def test_data_base_save(self):
         print('######## running save')
 
     def test_data_base_delete(self):
         print('######## running delete')
+
+    def test_non_data_base(self):
+        print('######## running non database test')

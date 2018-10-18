3d6815b3312dc95f95376c43f7b1161307f3a853
Irina Stolbova <ir.stolbova@gmail.co>
2018-10-19 01:50:31 +0200

    PyTest, mock remove_file for unittest with patch

--- test_unittest/test_remove_file.py
+++ test_unittest/test_remove_file.py
@@ -3,7 +3,7 @@ from remove_file import remove_file
 from unittest.mock import patch
 
 class BasicTestCase(TestCase):
-    def test_remove_file_is_called(self):
-        with patch('os.remove') as mock_remove:
-            remove_file('file')
-            mock_remove.assert_called_once_with('file')
+    @patch('os.remove')
+    def test_remove_file_is_called(self, mock_remove):
+        remove_file('file')
+        mock_remove.assert_called_once_with('file')
